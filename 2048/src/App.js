1b6accc50555d971674e1bd0811ee8c7048f4d65
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2022-12-05 21:50:10 -0500

    finished game functionality

--- 2048/src/App.js
+++ 2048/src/App.js
@@ -13,6 +13,8 @@ function App() {
     [0, 0, 0, 0]
   ])
 
+  const [gameOver, setGameOver] = useState(false)
+
   const leftkey = 37;
   const rightKey = 39;
   const upKey = 38;
@@ -42,15 +44,112 @@ function App() {
   //Initialize the grid with two random numbers 
   const initialize = () => {
     let newGrid = cloneDeep(data)
-    console.log(newGrid)
     addNumber(newGrid);
-    console.table(newGrid)
     addNumber(newGrid);
-    console.table(newGrid)
     setData(newGrid);
   }
 
-  const swipeLeft = () => {
+  //Rests the grid
+  const reset = () => {
+    setGameOver(false);
+    const resetGrid = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    addNumber(resetGrid);
+    addNumber(resetGrid);
+    setData(resetGrid);
+  };
+
+  const swipeUp = (sub) => {
+    let arr = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < 4; i++) {
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (arr[slow][i] === 0 && arr[fast][i] === 0) {
+          fast++;
+        } else if (arr[slow][i] === 0 && arr[fast][i] !== 0) {
+          arr[slow][i] = arr[fast][i];
+          arr[fast][i] = 0;
+          fast++;
+        } else if (arr[slow][i] !== 0 && arr[fast][i] === 0) {
+          fast++;
+        } else if (arr[slow][i] !== 0 && arr[fast][i] !== 0) {
+          if (arr[slow][i] === arr[fast][i]) {
+            arr[slow][i] = arr[slow][i] + arr[fast][i];
+            arr[fast][i] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(arr)) {
+      addNumber(arr);
+    }
+    if (sub)
+      return arr;
+    else
+      setData(arr);
+  }
+
+  const swipeDown = (sub) => {
+    let arr = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+
+    for (let i = 3; i >= 0; i--) {
+      let slow = arr.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (arr[slow][i] === 0 && arr[fast][i] === 0) {
+          fast--;
+        } else if (arr[slow][i] === 0 && arr[fast][i] !== 0) {
+          arr[slow][i] = arr[fast][i];
+          arr[fast][i] = 0;
+          fast--;
+        } else if (arr[slow][i] !== 0 && arr[fast][i] === 0) {
+          fast--;
+        } else if (arr[slow][i] !== 0 && arr[fast][i] !== 0) {
+          if (arr[slow][i] === arr[fast][i]) {
+            arr[slow][i] = arr[slow][i] + arr[fast][i];
+            arr[fast][i] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(arr) !== JSON.stringify(oldData)) {
+      addNumber(arr);
+    }
+    if (sub)
+      return arr;
+    else
+      setData(arr);
+  }
+
+  const swipeLeft = (sub) => {
     let oldGrid = data;
     let newArray = cloneDeep(data);
 
@@ -88,10 +187,13 @@ function App() {
     if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
       addNumber(newArray);
     }
-    setData(newArray)
+    if (sub)
+      return newArray;
+    else
+      setData(newArray);
   }
 
-  const swipeRight = () => {
+  const swipeRight = (sub) => {
     let oldData = data;
     let newArray = cloneDeep(data);
 
@@ -129,88 +231,11 @@ function App() {
     if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
       addNumber(newArray);
     }
-    setData(newArray)
+    if (sub)
+      return newArray;
+    else
+      setData(newArray);
   }
-
-  const swipeUp = () => {
-    let arr = cloneDeep(data);
-    let oldData = JSON.parse(JSON.stringify(data));
-    for (let i = 0; i < 4; i++) {
-      let slow = 0;
-      let fast = 1;
-      while (slow < 4) {
-        if (fast === 4) {
-          fast = slow + 1;
-          slow++;
-          continue;
-        }
-        if (arr[slow][i] === 0 && arr[fast][i] === 0) {
-          fast++;
-        } else if (arr[slow][i] === 0 && arr[fast][i] !== 0) {
-          arr[slow][i] = arr[fast][i];
-          arr[fast][i] = 0;
-          fast++;
-        } else if (arr[slow][i] !== 0 && arr[fast][i] === 0) {
-          fast++;
-        } else if (arr[slow][i] !== 0 && arr[fast][i] !== 0) {
-          if (arr[slow][i] === arr[fast][i]) {
-            arr[slow][i] = arr[slow][i] + arr[fast][i];
-            arr[fast][i] = 0;
-            fast = slow + 1;
-            slow++;
-          } else {
-            slow++;
-            fast = slow + 1;
-          }
-        }
-      }
-    }
-    if (JSON.stringify(oldData) !== JSON.stringify(arr)) {
-      addNumber(arr);
-    }
-    setData(arr)
-  }
-
-  const swipeDown = (dummy) => {
-    let arr = cloneDeep(data);
-    let oldData = JSON.parse(JSON.stringify(data));
-
-    for (let i = 3; i >= 0; i--) {
-      let slow = arr.length - 1;
-      let fast = slow - 1;
-      while (slow > 0) {
-        if (fast === -1) {
-          fast = slow - 1;
-          slow--;
-          continue;
-        }
-        if (arr[slow][i] === 0 && arr[fast][i] === 0) {
-          fast--;
-        } else if (arr[slow][i] === 0 && arr[fast][i] !== 0) {
-          arr[slow][i] = arr[fast][i];
-          arr[fast][i] = 0;
-          fast--;
-        } else if (arr[slow][i] !== 0 && arr[fast][i] === 0) {
-          fast--;
-        } else if (arr[slow][i] !== 0 && arr[fast][i] !== 0) {
-          if (arr[slow][i] === arr[fast][i]) {
-            arr[slow][i] = arr[slow][i] + arr[fast][i];
-            arr[fast][i] = 0;
-            fast = slow - 1;
-            slow--;
-          } else {
-            slow--;
-            fast = slow - 1;
-          }
-        }
-      }
-    }
-    if (JSON.stringify(arr) !== JSON.stringify(oldData)) {
-      addNumber(arr);
-    }
-    setData(arr)
-  }
-
 
   const handleKey = (event) => {
     switch (event.keyCode) {
@@ -229,6 +254,10 @@ function App() {
         swipeDown()
         break
     }
+    if (checkGameOver()) {
+      setGameOver(true);
+      alert("GAME OVER :( \nPress New Game to play again!")
+    }
   }
 
   useEffect(() => {
@@ -236,6 +265,26 @@ function App() {
   }, [])
 
   useKeyEvent('keydown', handleKey)
+
+  const checkGameOver = () => {
+    let checkerUp = swipeUp(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkerUp))
+      return false;
+
+    let checkerDown = swipeDown(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkerDown))
+      return false;
+
+    let checkerLeft = swipeLeft(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkerLeft))
+      return false;
+
+    let checkerRight = swipeRight(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkerRight))
+      return false;
+
+    return true;
+  };
 
   const Block = ({ num }) => {
     const { blockStyle } = style
@@ -247,31 +296,34 @@ function App() {
           background: getColors(num),
           color: num === 2 || num === 4 ? "#645B52" : "#F7F4EF",
         }}>
-        {num}
+        {num !== 0 ? num : ""}
       </div>
     )
   }
 
   return (
-    <div className='board' style={{
-      background: "#AD9D8F",
-      width: "max-content",
-      height: "max-content",
-      margin: "auto",
-      padding: 5,
-      borderRadius: 5,
-      marginTop: 10,
-      position: "relative",
-    }}>
-      {data.map((row, indexA) => {
-        return (
-          <div style={{ display: "flex" }} key={indexA}>
-            {row.map((digit, indexB) => (
-              <Block num={digit} key={indexB} />
-            ))}
-          </div>
-        );
-      })}
+    <div className='2048'>
+      <div className='board' style={{
+        background: "#AD9D8F",
+        width: "max-content",
+        height: "max-content",
+        margin: "auto",
+        padding: 5,
+        borderRadius: 5,
+        marginTop: 10,
+        position: "relative",
+      }}>
+        {data.map((row, indexA) => {
+          return (
+            <div style={{ display: "flex" }} key={indexA}>
+              {row.map((digit, indexB) => (
+                <Block num={digit} key={indexB} />
+              ))}
+            </div>
+          );
+        })}
+      </div>
+      <div className='resetBtn' onClick={reset}>New Game</div>
     </div>
   );
 }
